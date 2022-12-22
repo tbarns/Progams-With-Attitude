f4488d303950a6fc7deb289c5e8fb704f9021def
Timothy Barnaby <tbarnaby1@gmail.com>
2022-12-21 23:22:59 -0600

    added plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//why is this here? --v
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+//why--^
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. 
+//*****done?
 // TODO: Add CSS loaders and babel to webpack.
-
+//*****done?? is the css loader im using wrong */
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +21,37 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }), 
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
