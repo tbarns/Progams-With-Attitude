4ad7d890ea5e2b7d68ddf5176dede0dddc0e2cfd
Timothy Barnaby <tbarnaby1@gmail.com>
2022-12-27 14:12:29 -0600

    updated webpack and install.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,54 +1,72 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-//why is this here? --v
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-//why--^
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file. 
-//*****done?
-// TODO: Add CSS loaders and babel to webpack.
-//*****done?? is the css loader im using wrong */
 module.exports = () => {
   return {
     mode: 'development',
+    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
     },
+    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'Contact Cards'
       }),
-      new MiniCssExtractPlugin(),
+     
+      // Injects our custom service worker
       new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
-      }), 
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
